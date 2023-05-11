6795aab3a94ea65b7c724fde63b8390268961c95
sjankdev <stefanjanko.dev@gmail.com>
2023-05-11 20:24:43 +0200

    Created all pages and CSS for biling controller.

--- src/main/java/com/demo/hospitalmanagementtool/controllers/BillingController.java
+++ src/main/java/com/demo/hospitalmanagementtool/controllers/BillingController.java
@@ -1,13 +1,16 @@
 package com.demo.hospitalmanagementtool.controllers;
 
 import com.demo.hospitalmanagementtool.entities.Billing;
+import com.demo.hospitalmanagementtool.entities.Doctor;
 import com.demo.hospitalmanagementtool.service.AppointmentService;
 import com.demo.hospitalmanagementtool.service.BillingService;
 import com.demo.hospitalmanagementtool.service.DoctorService;
 import com.demo.hospitalmanagementtool.service.PatientService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -35,7 +38,7 @@ public class BillingController {
         return "billing/list";
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/{id}/details")
     public String getBillById(@PathVariable Long id, Model model) {
         Billing bill = billingService.getBillById(id);
         model.addAttribute("bill", bill);
@@ -55,21 +58,27 @@ public class BillingController {
     @PostMapping("/save")
     public String saveBill(@ModelAttribute("bill") Billing bill) {
         billingService.saveBill(bill);
-        return "redirect:/billing/";
+        return "redirect:/billing/list";
     }
 
     @GetMapping("/{id}/edit")
     public String editBill(@PathVariable Long id, Model model) {
-        Billing bill = billingService.getBillById(id);
-        model.addAttribute("bill", bill);
+        model.addAttribute("billing", billingService.getBillById(id));
+        model.addAttribute("doctors", doctorService.getAllDoctors());
+        model.addAttribute("patients", patientService.getAllPatients());
+        model.addAttribute("appointment", appointmentService.getAllAppointments());
         return "billing/edit";
     }
 
     @PostMapping("/{id}/update")
-    public String updateBill(@PathVariable Long id, @ModelAttribute("bill") Billing bill) {
-        billingService.updateBill(id, bill);
-        return "redirect:/billing/";
+    public String updateBill(@PathVariable Long id, @Valid @ModelAttribute("billing") Billing billing, BindingResult result) {
+        if (result.hasErrors()) {
+            return "billing/edit";
+        }
+        billingService.updateBill(id, billing);
+        return "redirect:/billing/list";
     }
+
 
     @GetMapping("/{id}/delete")
     public String deleteBill(@PathVariable Long id) {
