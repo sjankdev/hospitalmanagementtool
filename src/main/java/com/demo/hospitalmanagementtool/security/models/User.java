d0c739fca0a3426ad7e9ccac23dd77f67b8c1753
sjankdev <stefanjanko.dev@gmail.com>
2023-05-16 01:48:08 +0200

    Optimizing import and code formatting.

--- src/main/java/com/demo/hospitalmanagementtool/security/models/User.java
+++ src/main/java/com/demo/hospitalmanagementtool/security/models/User.java
@@ -50,4 +50,11 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
+    public User(String username, String firstName, String lastName, String email, String password) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
 }
