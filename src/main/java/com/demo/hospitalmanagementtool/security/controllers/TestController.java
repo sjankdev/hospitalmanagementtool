fd52ad45465d9095ec7edac5100de02d1ae14cf8
sjankdev <stefanjanko.dev@gmail.com>
2023-05-15 17:22:54 +0200

    Security is added, login and registration pages works fine. There is still a lot of stuff to be done around JWT and other authorization. For now, a base has been created that can be worked on.

---
+++ src/main/java/com/demo/hospitalmanagementtool/security/controllers/TestController.java
@@ -0,0 +1,53 @@
+package com.demo.hospitalmanagementtool.security.controllers;
+
+import com.demo.hospitalmanagementtool.security.token.services.UserDetailsImpl;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@Controller
+@RequestMapping("/api/test")
+public class TestController {
+    @GetMapping("/all")
+    public String allAccess() {
+        return "Public Content.";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public String userAccess() {
+        return "User Content.";
+    }
+
+    @GetMapping("/mod")
+    @PreAuthorize("hasRole('MODERATOR')")
+    public String moderatorAccess() {
+        return "Moderator Board.";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminAccess() {
+        return "Admin Board.";
+    }
+
+    @GetMapping("/homePage")
+    public String index(Model model, Principal principal) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl user = (UserDetailsImpl) authentication.getPrincipal();
+        long userId = user.getId();
+        model.addAttribute("userId", userId);
+
+        return "homepage";
+    }
+
+}
