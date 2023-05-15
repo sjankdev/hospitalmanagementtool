520151b666a1f78d668600f8bfdaee3ebfc8780d
sjankdev <stefanjanko.dev@gmail.com>
2023-05-15 23:40:26 +0200

    Added index page for switching through other pages.

---
+++ src/main/java/com/demo/hospitalmanagementtool/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package com.demo.hospitalmanagementtool.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+
+    @GetMapping()
+    public String index() {
+        return "index/index";
+    }
+
+}
