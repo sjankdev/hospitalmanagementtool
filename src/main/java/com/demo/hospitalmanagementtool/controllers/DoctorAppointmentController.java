2bddf921573b3b196f2ef0dab50918c001ab50d2
sjankdev <stefanjanko.dev@gmail.com>
2023-05-17 20:51:15 +0200

    Creating controller to displaying appointments for all doctors.

--- src/main/java/com/demo/hospitalmanagementtool/controllers/DoctorAppointmentController.java
+++ src/main/java/com/demo/hospitalmanagementtool/controllers/DoctorAppointmentController.java
@@ -4,7 +4,6 @@ import com.demo.hospitalmanagementtool.entities.Appointment;
 import com.demo.hospitalmanagementtool.entities.Doctor;
 import com.demo.hospitalmanagementtool.repository.DoctorRepository;
 import com.demo.hospitalmanagementtool.service.DoctorAppointmentCalendarService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Controller
 @RequestMapping("/doctorAppointments")
@@ -37,24 +34,19 @@ public class DoctorAppointmentController {
             @RequestParam(value = "month", required = false, defaultValue = "1") int month,
             Model model
     ) {
-        // Get doctor appointments for the specified doctor and month
         List<Appointment> doctorAppointments = calendarService.getDoctorAppointmentDatesByDoctorId(doctorId, year, month);
 
-        // Get calendar days for the specified month
         List<LocalDate> calendarDays = calendarService.getCalendarDays(year, month);
 
         Optional<Doctor> doctorOptional = doctorRepository.findById(doctorId);
         Doctor doctor = doctorOptional.orElseThrow(() -> new IllegalArgumentException("Doctor not found."));
 
-
-        // Prepare data for rendering in the Thymeleaf template
         model.addAttribute("doctorId", doctorId);
         model.addAttribute("doctor", doctor);
         model.addAttribute("calendarDays", calendarDays);
         model.addAttribute("appointments", doctorAppointments);
         model.addAttribute("monthYear", YearMonth.of(year, month).format(DateTimeFormatter.ofPattern("MMMM yyyy")));
 
-        // Calculate previous and next month values for navigation
         YearMonth currentMonth = YearMonth.of(year, month);
         YearMonth previousMonth = currentMonth.minusMonths(1);
         YearMonth nextMonth = currentMonth.plusMonths(1);
@@ -67,5 +59,37 @@ public class DoctorAppointmentController {
         return "doctor-appointment-calendar";
     }
 
+    @GetMapping("/doctor-appointment-calendar")
+    public String getAllDoctorAppointments(
+            @RequestParam(value = "year", required = false, defaultValue = "2023") int year,
+            @RequestParam(value = "month", required = false, defaultValue = "1") int month,
+            Model model
+    ) {
+        List<Doctor> doctors = doctorRepository.findAll();
+        Map<Doctor, List<Appointment>> doctorAppointmentsMap = new HashMap<>();
 
+        for (Doctor doctor : doctors) {
+            List<Appointment> doctorAppointments = calendarService.getDoctorAppointmentDatesByDoctorId(doctor.getId(), year, month);
+            doctorAppointmentsMap.put(doctor, doctorAppointments);
+        }
+
+        List<LocalDate> calendarDays = calendarService.getCalendarDays(year, month);
+
+        model.addAttribute("doctorAppointmentsMap", doctorAppointmentsMap);
+        model.addAttribute("calendarDays", calendarDays);
+        model.addAttribute("monthYear", YearMonth.of(year, month).format(DateTimeFormatter.ofPattern("MMMM yyyy")));
+
+        YearMonth currentMonth = YearMonth.of(year, month);
+        YearMonth previousMonth = currentMonth.minusMonths(1);
+        YearMonth nextMonth = currentMonth.plusMonths(1);
+
+        model.addAttribute("previousYear", previousMonth.getYear());
+        model.addAttribute("previousMonth", previousMonth.getMonthValue());
+        model.addAttribute("nextYear", nextMonth.getYear());
+        model.addAttribute("nextMonth", nextMonth.getMonthValue());
+
+        return "all-doctors-appointments";
+    }
 }
+
+
