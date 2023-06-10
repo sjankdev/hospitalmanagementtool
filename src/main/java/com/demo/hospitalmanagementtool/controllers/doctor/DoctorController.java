6be8121ae3b995d487b40b99e1d43c91fd066bf2
sjankdev <stefanjanko.dev@gmail.com>
2023-06-10 15:46:14 +0200

    Adding new links to the pages as well CSS.

--- src/main/java/com/demo/hospitalmanagementtool/controllers/doctor/DoctorController.java
+++ src/main/java/com/demo/hospitalmanagementtool/controllers/doctor/DoctorController.java
@@ -1,20 +1,22 @@
 package com.demo.hospitalmanagementtool.controllers.doctor;
 
+import com.demo.hospitalmanagementtool.entities.Appointment;
 import com.demo.hospitalmanagementtool.entities.AppointmentRequest;
 import com.demo.hospitalmanagementtool.entities.Doctor;
 import com.demo.hospitalmanagementtool.entities.Patient;
 import com.demo.hospitalmanagementtool.service.AppointmentRequestApprovalService;
 import com.demo.hospitalmanagementtool.service.AppointmentRequestService;
+import com.demo.hospitalmanagementtool.service.DoctorAppointmentCalendarService;
 import com.demo.hospitalmanagementtool.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Controller
@@ -29,6 +31,9 @@ public class DoctorController {
 
     @Autowired
     DoctorService doctorService;
+
+    @Autowired
+    private DoctorAppointmentCalendarService calendarService;
 
     @GetMapping("/index")
     public String index(Model model, Principal principal) {
@@ -84,5 +89,30 @@ public class DoctorController {
         } else {
             return "error";
         }
+    }
+
+    @GetMapping("/{doctorId}/appointments")
+    public String getDoctorAppointments(@PathVariable Long doctorId, @RequestParam(value = "year", required = false, defaultValue = "2023") int year, @RequestParam(value = "month", required = false, defaultValue = "1") int month, Model model, Principal principal) {
+
+        String username = principal.getName();
+
+        Doctor doctor = doctorService.getDoctorById(doctorId);
+
+        if (doctor != null && doctor.getUsername().equals(username)) {
+
+            List<Appointment> appointments = calendarService.getAppointmentsByDoctor(doctor);
+
+            LocalDate firstDayOfMonth = LocalDate.of(year, month, 1);
+            LocalDate lastDayOfMonth = firstDayOfMonth.withDayOfMonth(firstDayOfMonth.lengthOfMonth());
+
+            List<Appointment> appointmentsForMonth = calendarService.getAppointmentsForMonth(appointments, firstDayOfMonth, lastDayOfMonth);
+
+            Map<String, List<Appointment>> appointmentsByDate = calendarService.groupAppointmentsByDate(appointmentsForMonth);
+
+            calendarService.setModelAttributesDoctor(model, doctor, appointmentsByDate, year, month);
+
+        }
+        return "doctor-appointment-calendar";
+
     }
 }
