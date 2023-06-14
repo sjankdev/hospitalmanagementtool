d8d8822d4b18404039238b7bc2b73f67166abe15
sjankdev <stefanjanko.dev@gmail.com>
2023-06-14 17:30:03 +0200

    Added checkbox for filtering appointments, user can now select checkbox that will show/hide appointment requests from list.

--- src/main/java/com/demo/hospitalmanagementtool/controllers/admin/AdminAppointmentController.java
+++ src/main/java/com/demo/hospitalmanagementtool/controllers/admin/AdminAppointmentController.java
@@ -39,14 +39,6 @@ public class AdminAppointmentController {
         return "appointments/list";
     }
 
-    @GetMapping("/list-appointment-requests")
-    public String getAllAppointmentRequests(Model model) {
-        List<AppointmentRequest> appointmentRequests = appointmentRequestService.getAllAppointmentRequests();
-        model.addAttribute("appointmentRequests", appointmentRequests);
-        return "appointment-requests/list-appointment-requests";
-    }
-
-
     @GetMapping("/{id}/details")
     public String getAppointmentById(@PathVariable("id") Long id, Model model) {
         try {
